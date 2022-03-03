9b4413a50ad0e01982447bb7720ba2c040396447
maxwellarcher1 <maxwellarcher1@gmail.com>
2022-03-03 06:59:49 -0600

    changed baseUrl

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import router from './router'
 import store from './store/store'
 import axios from 'axios'
  
-axios.defaults.baseURL = 'http://127.0.0.1:8000'
+// axios.defaults.baseURL = 'http://127.0.0.1:8000'
+axios.defaults.baseURL = 'https://exq-ship.herokuapp.com'
+// https://exq-ship.herokuapp.com/
 // axios.defaults.headers.get['header-name'] = 'value'
 
 const app = createApp(App)
